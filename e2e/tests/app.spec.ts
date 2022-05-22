d7001b77469bbdc9b8cc4f461286fb314fd6e1a1
phau <1057021+ph4un00b@users.noreply.github.com>
2022-05-22 18:28:52 -0500

    chore(e2e): add base tests

--- e2e/tests/app.spec.ts
+++ e2e/tests/app.spec.ts
@@ -1,7 +1,43 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
+import { BASE_URL, expectLinkVisible, expectVisible } from "../utils";
 
-test('basic test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+test("root page", async ({ page }) => {
+  const find = page.locator.bind(page);
+  await page.goto(BASE_URL);
+
+  await expectVisible(find, "Stonks Climate!");
+  await expectLinkVisible(find, "Cryptos");
+  await expectLinkVisible(find, "Stonks");
+  await expectVisible(find, "Main!");
+});
+
+test("cryptos page", async ({ page }) => {
+  const find = page.locator.bind(page);
+  await page.goto(`${BASE_URL}/cryptos`);
+
+  await expectVisible(find, "Stonks Climate!");
+  await expectLinkVisible(find, "Cryptos");
+  await expectLinkVisible(find, "Stonks");
+  await expectVisible(find, "meme coins!");
+  await expectLinkVisible(find, "2021");
+  await expectLinkVisible(find, "2022");
+  expect(await find("select").inputValue()).toEqual("linear");
+});
+
+test("stocks page", async ({ page }) => {
+  const find = page.locator.bind(page);
+  await page.goto(`${BASE_URL}/stocks`);
+
+  await expectVisible(find, "Stonks Climate!");
+  await expectLinkVisible(find, "Cryptos");
+  await expectLinkVisible(find, "Stonks");
+  await expectLinkVisible(find, "Amazon");
+  await expectLinkVisible(find, "Alphabet");
+  await expectLinkVisible(find, "Apple");
+  await expectLinkVisible(find, "Microsoft");
+  await expectLinkVisible(find, "Meta");
+  await expectVisible(find, "Select stonk!");
+  await expectLinkVisible(find, "2021");
+  await expectLinkVisible(find, "2022");
+  expect(await find("select").inputValue()).toEqual("linear");
 });
