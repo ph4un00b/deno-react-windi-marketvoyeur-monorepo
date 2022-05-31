99bf6aea1fa289c22668e47c63de5fdcc8bec76a
phau <1057021+ph4un00b@users.noreply.github.com>
2022-05-31 13:08:41 -0500

    chore(spa): drop unused deps

--- spa/src/components/molecules/DefaultContent.tsx
+++ spa/src/components/molecules/DefaultContent.tsx
@@ -2,7 +2,7 @@ import { React, tw } from "../../client_deps.ts";
 
 export function DefaultContent() {
   return (
-    <main className={tw`bg-emerald-100 text-green-600`}>
+    <main className={tw`bg-green-500 text-green-600`}>
       <p>Main!</p>
     </main>
   );
